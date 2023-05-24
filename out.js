3f198913f5ef0c6120bfbe839abe3da38c4e0801
tkx4 <90717465+tkx4@users.noreply.github.com>
2023-05-24 14:46:46 +0300

    Add files via upload

---
+++ out.js
@@ -0,0 +1,12 @@
+let n1 = 7;
+let n2 = 4;
+
+console.log(n1+n2);
+
+
+const name = prompt("Enter your name : ");
+
+const age = prompt("Enter your age : ");
+
+
+console.log('your name is : ${name}\nyour age is : ${age}');
